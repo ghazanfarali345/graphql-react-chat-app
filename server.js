4a3ef8d399bb2ea650afcd7478fc7158a91cd6be
Ghazanfar Ali <ghazanfarmalik345@gmail.com>
2022-02-07 17:53:36 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { sequelize } = require("./models");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
+
+sequelize
+  .authenticate()
+  .then((res) => console.log("database connected"))
+  .catch((err) => console.log(err));
